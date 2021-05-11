ca54b866e2d7d91874817e698453443f8ab824e7
Liam <liambrockwell46@gmail.com>
2021-05-11 17:56:14 +0100

    add functions to recognise email

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 document.querySelector('form').addEventListener('submit', function(e){
  e.preventDefault()
 })
+
+
+
+ user = {
+    email:'liam@23com',
+    password:'liam',
+    myFunc(email=user.email,password=user.password) {
+       
+    }
+}
+const logIn = (userEm) =>{
+    if(userEm != user.email){
+        console.log('go away');
+    } else{
+        console.log('hiiii');
+    }
+ }
+
+ const formVal = () => {
+   form = document.querySelector('form')
+   usrEmail = form.elements[0];
+   usrEmailInp = 'liam@23com';
+   return usrEmailInp
+ }
+ logIn(formVal())
+//user.myFunc()
